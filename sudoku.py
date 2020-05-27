4eb22dae236c69da8b825122acbe3b9a277054e9
DennisTjeerdsma <dennistjeerdsma@hotmail.com>
2020-05-27 17:37:26 +0200

    setters and getters, Region class, tests

--- sudoku.py
+++ sudoku.py
@@ -1,17 +1,20 @@
-class Cell:
+class Cell(object):
     def __init__(self, row, column, editable=True, value=0):
-        self.value: int = value             # The value of the object cell
-        self.row: int = row                 # The row of the cell object, rows can have a value of 0 - 8
+        self.row:int = row               # The row of the cell object, rows can have a value of 0 - 8
         self.column: int = column           # The column of the cell object, columns can have a value of 0-8
-        self._editable = editable            # Editable property of the cell, if editable is true, the value can be adjusted
+        self.editable = editable            # Editable property of the cell, if editable is true, the value can be adjusted
+        self.value: int = value             # The value of the object cell
+        self.possibleValues = set()
 
     @property
     def row(self) -> int:
+        # property of cell retrieving the private _row index of the cell instance
         return self._row
 
     @row.setter
     def row(self, r) -> int:
-        if (r < 0 or r > 8): raise ValueError("Column value must be a value of 0 or higher or 8 or lower")
+        # property of cell setting the private _row index of the cell instance, value must be between 0 - 8, hard coded for 9x9 sudoku
+        if (r < 0 or r > 8): raise ValueError("Row value must be a value of 0 or higher or 8 or lower")
         self._row = r
 
     @property
@@ -20,14 +23,90 @@ class Cell:
 
     @column.setter
     def column(self, c) -> int:
+        # property of cell setting the private _column index of the cell instance, value must be between 0 - 8, hard coded for 9x9 sudoku
         if (c < 0 or c > 8): raise ValueError("Column value must be a value of 0 or higher or 8 or lower ")
         self._column = c
 
     @property
     def value(self) -> int:
-        return self_value
+        # propery of cell return the value variable
+        return self._value
 
     @value.setter
-    def value(self, i) -> int:
-        
+    def value(self, v) -> int:
+        # Value setter of the cell class, throws an error if the cell is not editable or if the value is not from 1 to 9
+        if self.editable is False and v != 0:
+            raise AttributeError("Cell is not editable")
+        elif v < 0 or v > 9:
+            raise ValueError(f"Incorrect value {v} not between or equal to <1,9>")
+        else: self._value = v
 
+    @property
+    def editable(self) -> bool:
+        """ Return a boolean value based on if the cell is editable"""
+        return self._editable
+
+    @editable.setter
+    def editable(self, e) -> bool:
+        self._editable = e
+
+class Region(object):
+    """
+    Class holding a list of cells that have unique values. Can be a:
+    - row
+    - column
+    - box
+    The type of the class is not nexecarry
+    """
+    def __init__(self):
+        self._cells = []
+
+    @property
+    def cells(self):
+        return self._cells
+
+class Sudoku(object):
+    """
+    Class holding the sudoku game object. 
+    Cells are generated based on the 2d input puzzle.
+    """
+    def __init__(self, puzzle):
+        self._puzzle = puzzle# Value holding a 2d array of the to be solved puzzle
+        self._cells = []
+    
+    @property
+    def puzzle(self):
+        return self._puzzle
+
+class SudokuSerializer(object):
+    """
+    Sudoku initializer takes an input of a sudoku board and turns it into a 2d array for the sudoku class
+    For now, sudoku initializer only takes a 2d string as input, in the future arrays and 1d string will be incldued
+    """
+    @staticmethod
+    def create_from_string(puzzle):
+        if isinstance(puzzle, str):
+            l = []
+            for row in puzzle.split():
+                row_arr = [int(i) for i in row]
+                l.append(row_arr)
+            return Sudoku(l)
+
+if __name__ == '__main__':
+    cell = Cell(8,4, False, 0)
+    
+    sudoku_string = """
+                    500009100
+                    000062508
+                    010008764
+                    065013020
+                    240007010
+                    000000000
+                    070000056
+                    058700300
+                    000040800         
+                    """
+    print("TEST")
+
+    sudoku = SudokuSerializer.create_from_string(sudoku_string)
+      
--- test_sudoku.py
+++ test_sudoku.py
@@ -1,23 +1,54 @@
 import unittest
 
 from sudoku import Cell as CellClass
+from sudoku import Sudoku as SudokuClass
+from sudoku import SudokuSerializer
 
 class Test(unittest.TestCase):
     def setUp(self):
         self.cell1 = CellClass(4, 3, True, 1)
-
+        self.cell2 = CellClass(3, 2, False, 4)
+        self.sudoku1 = SudokuSerializer().create_from_string(
+                    """
+                    500009100
+                    000062508
+                    010008764
+                    065013020
+                    240007010
+                    000000000
+                    070000056
+                    058700300
+                    000040800         
+                    """)
     def test_1_cell_exception_handling(self):
-        self.assertRaises(ValueError, CellClass, 9, 4)
+        self.assertRaises(ValueError, CellClass, 10, 4)
         self.assertRaises(ValueError, CellClass, -1, 3)
         self.assertRaises(ValueError, CellClass, 2, -1)
         self.assertRaises(ValueError, CellClass, 2, 9)
-        print("##-Correct Cell Exception handling-##\n")
 
     def test_2_cell_value_assignment(self):
         self.assertEqual(self.cell1.row, 4)
         self.assertEqual(self.cell1.column, 3)
         self.assertEqual(self.cell1.value, 1)
-        print("##-Correct Row and column assignment-##")
+
+    def test3_convert_sudoku_from_string_to_arr(self):
+        self.assertEqual(self.sudoku1.puzzle, [[5,0,0,0,0,9,1,0,0],
+                                                [0,0,0,0,6,2,5,0,8],
+                                                [0,1,0,0,0,8,7,6,4],
+                                                [0,6,5,0,1,3,0,2,0],
+                                                [2,4,0,0,0,7,0,1,0],
+                                                [0,0,0,0,0,0,0,0,0],
+                                                [0,7,0,0,0,0,0,5,6],
+                                                [0,5,8,7,0,0,3,0,0],
+                                                [0,0,0,0,4,0,8,0,0]])
+
+    def test4_assigning_cell_values(self):
+        self.assertEqual(self.cell1.value, 1)
+        self.assertTrue(self.cell1.editable)
+        self.assertEqual(self.cell2.value, 4)
+        self.assertFalse(self.cell2.editable)
+        self.assertRaises(AttributeError, self.cell2.value, 2)
+        self.assertEqual(self.cell1.value(5), 5)
         
 if __name__ == '__main__':
     unittest.main()
